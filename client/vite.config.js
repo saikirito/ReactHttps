d861d32baad5caad0ac2e3a9ca25a1f9a7015d7c
saikirito <saikirito1@gmail.com>
2024-12-20 21:07:37 +0530

    updated port to 80

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import fs from 'fs';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    port: 80,
+    host: '0.0.0.0',
     https: {
       key: fs.readFileSync('./key.pem'),
       cert: fs.readFileSync('./cert.pem')
